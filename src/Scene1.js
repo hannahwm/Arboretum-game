8361f2533924cf8e31fbff4b56f65852330fa533
Hannah <ha.moore@northeastern.edu>
2019-08-23 16:56:46 -0400

    added nuts

--- src/Scene1.js
+++ src/Scene1.js
@@ -1,7 +1,10 @@
 import Phaser, { Scene } from 'phaser';
 import Level from './Level';
+import gameConfig from './gameConfig';
 
 let onLadder = false;
+
+const global = {}
 
 class Scene1 extends Scene {
   constructor() {
@@ -14,10 +17,12 @@ class Scene1 extends Scene {
 
   preload() {
     this.load.image('sky', 'interactive/2019/08/phaser-game/assets/sky.png');
+    this.load.image('middle', 'interactive/2019/08/phaser-game/assets/middle.png');
+    this.load.image('background', 'interactive/2019/08/phaser-game/assets/background.png');
     this.load.image('ground', 'interactive/2019/08/phaser-game/assets/ground.png');
     this.load.image('platform', 'interactive/2019/08/phaser-game/assets/platform.png');
     this.load.image('ladder', 'interactive/2019/08/phaser-game/assets/ladder.png');
-    this.load.image('background', 'interactive/2019/08/phaser-game/assets/background.png');
+    this.load.image('nut', 'interactive/2019/08/phaser-game/assets/nut.png');
     this.load.spritesheet('dude',
       'interactive/2019/08/phaser-game/assets/dude.png',
       { frameWidth: 32, frameHeight: 48 });
@@ -35,8 +40,11 @@ class Scene1 extends Scene {
     this.createBackground();
     this.createCursor();
     this.createJumpButton();
+    this.createNuts();
     this.throwNut();
-    this.cameras.main.setBounds(0, 0, 1024, 2048);
+    this.cameras.main.setBounds(0, 0, this.map.width, this.map.height);
+    this.physics.world.setBounds(0, 0, this.map.getBounds().width,
+      this.map.height + this.player.height);
     this.cameras.main.startFollow(this.player, true, 0.5, 0.5, 0, 180);
 
     this.scoreText = this.add.text(16, 50, 'score: 0', {
@@ -47,23 +55,52 @@ class Scene1 extends Scene {
     this.gameOverText.setOrigin(0.5);
     this.gameOverText.visible = false;
     this.gameOverText.setScrollFactor(0);
+    this.gameOverText.setDepth(5);
   }
 
   createBackground() {
-    this.map = this.add.image(0, 0, 'background').setOrigin(0).setScrollFactor(1);
-    this.map.setDepth(0);
+    this.map = this.add.image(0, 0, 'background');
+    this.map.setDepth(2);
+    this.map.setOrigin(0, 0.05);
+    // this.map.setScale(0.75);
+
+    this.middle = this.add.image(0, 0, 'middle');
+    this.middle.setDepth(1);
+    this.middle.setOrigin(0, 0.05);
+    // this.middle.setScale(0.75);
+
+    this.sky = this.add.image(0, 0, 'sky');
+    this.sky.setDepth(0);
+    this.sky.setOrigin(0, 0);
+    // this.sky.setScale(0.5);
+
+    const gameWidth = parseFloat(this.map.getBounds().width);
+    const windowWidth = gameConfig.width;
+    const bgWidth = this.map.getBounds().width;
+    const middleWidth = this.middle.getBounds().width;
+    const skyWidth = this.sky.getBounds().width;
+
+    this.map.setScrollFactor((bgWidth - windowWidth) / (gameWidth - windowWidth));
+    this.middle.setScrollFactor((middleWidth - windowWidth) / (gameWidth - windowWidth));
+    this.sky.setScrollFactor((skyWidth - windowWidth) / (gameWidth - windowWidth));
   }
 
   createGround() {
     this.ground = this.physics.add.staticGroup();
-    this.ground.create(400, 680, 'ground');
-    this.ground.setDepth(1);
+    this.ground.create(400, 610, 'ground');
+    this.ground.setDepth(3);
+
+    this.ground.children.iterate((child) => {
+      child.setScale(2);
+      child.setSize(4200, 63);
+      child.setOffset(0, -18);
+    });
   }
 
   createPlatforms() {
     this.platforms = this.physics.add.staticGroup();
-    this.platforms.create(480, 540, 'platform');
-    this.platforms.setDepth(3);
+    this.platforms.create(480, 480, 'platform');
+    this.platforms.setDepth(4);
 
     this.platforms.children.iterate((child) => {
       child.setSize(160, 33);
@@ -73,8 +110,8 @@ class Scene1 extends Scene {
 
   createLadders() {
     this.ladders = this.physics.add.staticGroup();
-    this.ladders.create(300, 510, 'ladder');
-    this.ladders.setDepth(1);
+    this.ladders.create(300, 460, 'ladder');
+    this.ladders.setDepth(3);
 
     this.ladders.children.iterate((child) => {
       child.setSize(40, 250);
@@ -84,17 +121,16 @@ class Scene1 extends Scene {
 
 
   createPlayer() {
-    this.player = this.physics.add.sprite(100, 610, 'dude');
+    this.player = this.physics.add.sprite(100, 550, 'dude');
     this.player.setSize(25, 40);
     this.player.setOffset(3, 7);
     this.player.setBounce(0.2);
     this.player.setGravityY(300);
-    this.player.setDepth(2);
+    this.player.setDepth(3);
+    this.player.setCollideWorldBounds(true);
     this.physics.add.collider(this.player, this.platforms);
     this.physics.add.collider(this.player, this.ground);
     this.physics.add.overlap(this.player, this.ladders, this.detectOverlap);
-
-
 
     this.anims.create({
       key: 'left',
@@ -126,9 +162,9 @@ class Scene1 extends Scene {
       repeat: -1,
     });
 
-    this.enemy = this.physics.add.sprite(420, 500, 'squirrel');
+    this.enemy = this.physics.add.sprite(420, 440, 'squirrel');
     this.enemy.anims.play('squirrel');
-    this.enemy.setDepth(2);
+    this.enemy.setDepth(3);
     this.enemy.setGravityY(300);
 
     this.physics.add.collider(this.player, this.enemy, this.touchEnemy, null, this);
@@ -173,8 +209,41 @@ class Scene1 extends Scene {
     // this.input.on('pointerdown', () => this.scene.start('preload'));
   }
 
+  createNuts() {
+    global.nuts = this.physics.add.group();
+    // this.physics.add.collider(this.nuts, this.platforms);
+    this.physics.add.collider(this.player, this.nuts);
+    // , this.hitBomb, null, this
+
+    global.nutParticles = this.add.particles('nut');
+    global.nutEmitter = global.nutParticles.createEmitter({
+      x: 300,
+      y: 500,
+      lifespan: 2000,
+      speedX: -10,
+      speedY: 10,
+      quantity: 3,
+      blendMode: 'ADD',
+    });
+  }
+
   throwNut() {
     console.log("throw nut");
+    const nut = global.nuts.create(200, 400, 'nut');
+    nut.setDepth(5);
+    // .setVelocity(Phaser.Math.Between(-200, 200), 20);
+
+    // const nuts = global.nuts.add.particles('nut');
+    // nuts.setDepth(5);
+    // const emitter = nuts.createEmitter({
+    //   x: 300,
+    //   y: 500,
+    //   lifespan: 2000,
+    //   speedX: -10,
+    //   speedY: 10,
+    //   quantity: 3,
+    //   blendMode: 'ADD',
+    // });
   }
 
   // ========================================================
